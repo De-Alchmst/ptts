c9f2a18a63069a31a009907723487d6ba86d98ef
De-Alchmst <PolakJ_37@proton.me>
2024-08-23 15:23:17 -0700

    stuff?

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ A nice way to format text to be shown inside terminal / with monospace font.
 Both more clasicall and vi-based keybindings are supported in the tui.
 Metadata can be shown by pressing 'm'
 
-Currently under developement. `sourcecnfg`, `sourcetxt`, and `sourceall`
-are not implemented yet, rest should work.
+## dependencies
+- xelatex (optional, but needed for pdf)
+- Visual Studio Build Tools (if compiling on Windows (should come with the compiler??))
 
-PDF almost works, LaTeX also works, but is not at all readable.
-
+## fonts
 Fonts are a bit finicky, so when in doubt, copy fonts next to your document.
 
 Font names should be as follows:
@@ -20,4 +20,5 @@ Font names should be as follows:
 <fontname>-BoldItalic.{otf|ttf}
 ```
 
+## attributions
 ptts comes with the [HACK](https://sourcefoundry.org/hack/) font.
---
+++ install-windows.ps1
@@ -0,0 +1,90 @@
+$local = $false
+
+# args
+if ($args.length -gt 0) {
+	if ($args.length -eq 1 -and $args[0] -eq "-l") {
+		$local = $true
+	} else {
+		echo "Usage: install-windows.ps1 {-l}"
+		echo "  -l  Install locally"
+		exit 1
+	}
+}
+
+#test for admin
+if (-not $local) {
+	# a hack??
+	if ((fltmc.exe).Count -eq 3) {
+		echo "Cannot install globally without admin privileges"
+		echo "Run install-linux.sh -l to install locally"
+		exit 1
+	}
+}
+
+# dependencies
+if (-not (get-command crystal -ErrorAction silentlyContinue)) {
+	echo "crystal nont found, can not compile"
+	exit 1
+}
+
+if (-not (get-command shards -ErrorAction silentlyContinue)) {
+	echo "shards not found for some reason found, can not compile"
+	exit 1
+}
+
+cd src
+
+echo "Installing shards..."
+shards install
+if (-not $?) {
+   echo "shards install failed, aborting installation"
+   exit 1
+}
+
+echo "Compiling..."
+crystal build ptts.cr
+if (-not $?) {
+   echo "crystal build failed, aborting installation"
+   exit 1
+}
+
+# install
+$binDir = "C:\ProgramFiles\ptts\"
+$dataDir = "C:\ProgramFiles\ptts\fonts\"
+
+if ($local) {
+   $binDir = "$env:LOCALAPPDATA\ptts\"
+   $dataDir = "$env:LOCALAPPDATA\ptts\fonts\"
+}
+
+echo "making directories"
+new-item -path $binDir -itemType directory -force -ErrorAction silentlyContinue
+new-item -path $dataDir -itemType directory -force -ErrorAction silentlyContinue
+
+echo "copying files"
+cp ptts $binDir
+cp ..\data\Hack\* $dataDir
+
+echo "adding to $PATH"
+
+if ($local) {
+	$userPath = [Environment]::GetEnvironmentVariable("PATH", [EnvironmentVariableTarget]::User)
+	if ($userPath -notcontains $binDir) {
+		[Environment]::SetEnvironmentVariable("PATH", "$userPath;$binDir", [EnvironmentVariableTarget]::User)
+	}
+} else {
+	$machinePath = [Environment]::GetEnvironmentVariable("PATH", [EnvironmentVariableTarget]::Machine)
+	if ($machinePath -notcontains $binDir) {
+		[Environment]::SetEnvironmentVariable("PATH", "$machinePath;$binDir", [EnvironmentVariableTarget]::Machine)
+	}
+}
+
+cd ..
+
+echo "done!"
+
+# check for latex
+if (-not (get-command shards -ErrorAction silentlyContinue)) {
+	echo "WARNING: xelatex not found, exporting to pdf won't work!"
+	exit 1
+}
